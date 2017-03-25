1b015a66e50742c9d5fa8edfaef26f3bcb08301a
EmanuelDubor <emanueldubora@hotmail.com>
2017-03-25 19:22:09 -0300

    Done: Creando una noticia

--- src/frontend/bootstrap.js
+++ src/frontend/bootstrap.js
@@ -4,15 +4,18 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 
 import AppComponent from "./app/components/app.component";
 import PostComponent from "./app/components/post.component";
+import NewPostComponent from "./app/components/newPost.component";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     declarations: [
         AppComponent,
-        PostComponent
+        PostComponent,
+        NewPostComponent
     ],
     bootstrap: [AppComponent]
 })
